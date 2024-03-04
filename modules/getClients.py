bbd5ea63c8a338f8b0a5e25168309c7842828271
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-04 08:56:38 -0500

    RECUERDE GUARDAR YA FUNCIONA SIEMPRE FUNCIONÓ PERO NO LE DI CTRL+S

--- modules/getClients.py
+++ modules/getClients.py
@@ -1,4 +1,41 @@
-from jardineríaCampus.storage.cliente import clientes
+import storage.cliente as cli
 
-def searchName():
-    print(clientes)
+def search():
+    clienteNames= list()
+    for val in cli.clientes:
+        codigoName=dict({
+        "codigo_cliente": val.get("codigo_cliente"),
+        "nombre_cliente": val.get ("nombre_cliente")
+     })
+        clienteNames.append(codigoNames)
+        return clienteNames
+    
+
+def getOneClienteCodigo(codigo):
+    clienteNames= list()
+    for val in cli.clientes:
+        if (val.get("codigo_cliente")==codigo):
+            return{
+                "codigo_cliente": val.get("codigo_cliente"),
+                "nombre_cliente": val.get("nombre_cliente")
+            }
+
+
+def  getAllClientCreditCiudad(limiteCredit,ciudad):
+    clienteCredit=list()
+    for val in cli.clientes:
+        if(val.get("limite_credito")>=limiteCredit and val.get("ciudad")==ciudad):
+            clienteCredit.append(val)
+    return clienteCredit
+
+def getAllPaisRegionCiudad (pais,region=None,ciudad=None):
+    clientZone = list()
+    for val in cli.clientes:
+        if(
+            val.get('pais')==pais and
+            (val.get('region')==region or val.get('region')==None) or
+            (val.get('ciudad')==ciudad or val.get('ciudad')==None)    
+        ):
+            clientZone.append(val)
+    return clientZone        
+
