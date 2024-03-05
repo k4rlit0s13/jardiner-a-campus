e92c578f9253ec56689f5349725ad5407092163d
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-05 13:11:30 -0500

    filtro para clientes con codigo par

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import modules.getClients as cliente
 
-print(cliente.obtener_informacion_clientes ())
+print(cliente.GetAllCodePar ())
--- modules/getClients.py
+++ modules/getClients.py
@@ -41,7 +41,7 @@ def getAllPaisRegionCiudad (pais,region=None,ciudad=None):
 
 
 
-#filtro par obtener nombre con la direccion numero 2 de los clientes
+#filtro para obtener nombre con la direccion numero 2 de los clientes
 def obtener_informacion_clientes():
     clientes_info = []
     for cliente in cli.clientes:
@@ -51,3 +51,20 @@ def obtener_informacion_clientes():
         }
         clientes_info.append(info)
     return clientes_info
+
+
+#filtro para obtener los nombres de los clientes que contengan un código con número par
+def GetAllCodePar ():
+    clientes_info = []
+    for cliente in cli.clientes:
+        codigo_cliente = cliente.get("codigo_cliente")
+        if codigo_cliente % 2 == 0:  # Verifica si el código del cliente es par
+            info = {
+                "nombre_cliente": cliente.get("nombre_cliente"),
+                "codigo_cliente": codigo_cliente
+            }
+            clientes_info.append(info)
+    return clientes_info
+
+
+#filtro que permita buscar clientes cuyos nombres coincidan parcialmente con un término de búsqueda proporcionado
