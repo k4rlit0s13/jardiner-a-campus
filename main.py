7258fa6048cd456ef379856a5002d9dcd396b119
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-06 15:20:50 -0500

    filtro para devolver un listado con los distintos estados por los que puede pasar un pedido

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import modules.getClients as cliente
 import modules.getOficina as oficina
 import modules.getEmpleados as empleado
-
+import modules.getPedido as pedido
 
 #print(cliente.GetAllNamesSpain())
 
-print(cliente.GetAllNamesSpain())
+print(pedido.obtener_estados_pedidos())
--- modules/getPedido.py
+++ modules/getPedido.py
@@ -1,3 +1,12 @@
 import storage.pedido as pe
 
 #filtro para devolver un listado con los distintos estados por los que puede pasar un pedido
+
+def obtener_estados_pedidos():
+    estados = set()  # Utilizamos un conjunto para asegurarnos de obtener estados únicos
+    for pedido in pe.pedido:  
+        estado = pedido.get("estado")
+        if estado not in estados:
+            estados.add(estado)
+    return estados
+
