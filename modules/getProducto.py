45d88b4d68861b694e137f036a010862c25f6d96
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-03-11 14:09:26 -0500

    avancez menu productos

--- modules/getProducto.py
+++ modules/getProducto.py
@@ -1,15 +1,47 @@
 import storage.producto as pro
 from tabulate import tabulate
 
+#Obtener todos los productos
+def getAllProducts():
+        AllproductsProducts=[]
+        for val in pro.producto:
+                AllproductsProducts.append({
+                        "Código del Producto": val.get("codigo_producto"),
+                        "Nombre": val.get("nombre"),
+                        "Gama": val.get("gama"),
+                        "Dimensiones": val.get("dimensiones"),
+                        "Proveedor": val.get("proveedor"),
+                        "Descripción": val.get("descripcion"),
+                        "Cantidad en stock": val.get("cantidad_en_stock"),
+                        "Precio de Venta": val.get("precio_venta")
+})
+        return AllproductsProducts
 
 
+#Obtener la gama de cada producto 
+def getAllProductsGama():
+        AllProductsGama=[]
+        for val in pro.producto:
+                AllProductsGama.append({
+                        "Código del Producto": val.get("codigo_producto"),
+                        "Nombre": val.get("nombre"),
+                        "Gama": val.get("gama"),
+                        "Precio de Venta": val.get("precio_venta")
+})
+        return AllProductsGama
 
 
-
-
-
-
-
+#Obtener todas las catidades en stock 
+def getAllProductsStock():
+        AllProductsStock=[]
+        for val in pro.producto:
+                AllProductsStock.append({
+                        "Código del Producto": val.get("codigo_producto"),
+                        "Nombre": val.get("nombre"),
+                        "Dimensiones": val.get("dimensiones"),
+                        "Cantidad en stock": val.get("cantidad_en_stock"),
+})
+        return AllProductsStock
 
 
 
@@ -29,12 +61,24 @@ def menu():
 ██║     ██║  ██║╚██████╔╝██████╔╝╚██████╔╝╚██████╗   ██║   ╚██████╔╝███████║
 ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝  ╚═════╝   ╚═╝    ╚═════╝ ╚══════╝
                                                                             
-        1.
-        2.
-        3.
+        1. Obtener todos los productos
+        2. Obtener la gama de cada producto 
+        3. Obtener todas las catidades en stock 
         4.
               
         0. Salir al menu principal
 
 """)
+        opcion=int(input("\nSeleccione una de las opciones: "))
+        
+        if(opcion==1):
+            print(tabulate(getAllProducts(), headers="keys",tablefmt="grid"))
+        if(opcion==2):
+            print(tabulate(getAllProductsGama(), headers="keys",tablefmt="grid"))
+        if(opcion==3):
+            print(tabulate(getAllProductsStock(), headers="keys",tablefmt="grid"))
+
+
+        if(opcion==0):
+            break
 
